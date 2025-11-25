af1d4d658aed79f517e5dfe23e14722af550a595
Hiep Nguyen <nguyendunghiep20012003@gmail.com>
2025-11-26 00:18:47 +0700

    add health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,15 @@
-import {Application} from 'express';
-import {createVerifyGatewayRequest} from '@hiep20012003/joblance-shared';
-import {config} from '@chats/config';
-import {initialContextMiddleware} from '@chats/middlewares/context.middleware';
+import { Application } from 'express';
+import { createVerifyGatewayRequest } from '@hiep20012003/joblance-shared';
+import { config } from '@chats/config';
+import { initialContextMiddleware } from '@chats/middlewares/context.middleware';
 
-import {healthRoutes} from './health.route';
-import {messageRoutes} from './message.route';
+import { healthRoutes } from './health.route';
+import { messageRoutes } from './message.route';
 
 const BASE_PATH = '/api/v1';
 
 export const appRoutes = (app: Application) => {
-    app.use(initialContextMiddleware);
-    app.use('', healthRoutes.routes());
-    app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), messageRoutes.routes());
+  app.use('', healthRoutes.routes());
+  app.use(initialContextMiddleware);
+  app.use(BASE_PATH, createVerifyGatewayRequest(`${config.GATEWAY_SECRET_KEY}`), messageRoutes.routes());
 };
